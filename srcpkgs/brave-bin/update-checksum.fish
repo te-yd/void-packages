80a3bc556dd3c9f9bdac2535296862a1864a1476
Rithick S <rithick.s49@gmail.com>
2026-04-17 21:27:56 +0530

    Update to Brave 1.89.x

--- srcpkgs/brave-bin/update-checksum.fish
+++ srcpkgs/brave-bin/update-checksum.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/fish
 
 echo "Update version checksum"
-set VERSION (cat template | rg 'version=' | cut -d "=" -f 2)
+set VERSION (cat template | grep 'version=' | cut -d "=" -f 2)
 
 set DOWNLOAD_URL https://github.com/brave/brave-browser/releases/download/v{$VERSION}/brave-browser_{$VERSION}_amd64.deb.sha256
 set CHECKSUM (wget "$DOWNLOAD_URL" -q --output-document - | cut -d " " -f 1)
